b8f7f0b85803ac3ee94dca7a5048b6b6b7a1e59a
cod3cat <25690316+cod3cat@users.noreply.github.com>
2019-09-17 19:50:27 +0530

    Adding multiple classes

--- Composition/src/com/cod3cat/composition/Main.java
+++ Composition/src/com/cod3cat/composition/Main.java
@@ -3,6 +3,15 @@ package com.cod3cat.composition;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dimensions dimensions = new Dimensions(20, 20 ,5);
+	    Case theCase = new Case("220B","Dell", "240", dimensions);
+
+	    Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+	    Motherboard motherboard = new Motherboard("BJ-220", "ASUS", 4, 2, "v2.44");
+
+	    PC thePc = new PC(theCase,theMonitor,motherboard);
+	    thePc.getMonitor().drawPixelAt(1500,1200, "Red");
+		thePc.getMotherboard().loadProgram("Windows 10");
+		thePc.getTheCase().pressPowerButton();
     }
 }
